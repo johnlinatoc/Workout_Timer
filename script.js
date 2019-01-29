1ee773a2450c01124038f040a6700d8a65da0f19
John Linatoc <johnlinatoc1227@gmail.com>
2019-01-28 19:43:21 -0600

    adding npm

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 let countdown;
 const timerDisplay = document.getElementById("here");
+const btn = document.getElementById("reset");
 
 
-function timer(seconds) {
- console.log(seconds);
- displayTime(seconds);    
-    
- countdown = setInterval(() => {
- seconds--;
+
+//first set timer
+function setTimer(seconds) {
+    console.log(seconds);
+    displayTime(seconds);
+
+    countdown = setInterval(() => {
+        seconds--;
 
 
-     //stop timer
-    if (seconds < 0) {
-         clearInterval(countdown);
-         return;
+        //stop timer
+        if (seconds < 0) {
+            clearInterval(countdown);
+            return;
         }
-        
-    //display time
+
+        //display time
         displayTime(seconds);
         console.log(seconds);
     }, 1000);
 }
 
+//resets fields after entering
+document.box.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const time = this.num.value;
+    setTimer(time);
+    this.reset();
+})
+
+
+
+//reset button
+btn.addEventListener("click", function (e) {
+    clearInterval(countdown);
+    timerDisplay.innerHTML = "0:00";
+});
+
+
+
+//timer display
 function displayTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
@@ -30,9 +52,3 @@ function displayTime(seconds) {
     document.title = display;
 }
 
-document.box.addEventListener("submit", function(e){
-    e.preventDefault();
-    const time = this.num.value;
-    timer(time);
-    this.reset();
-})
